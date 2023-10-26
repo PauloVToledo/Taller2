4dd626c38fcd5c6ee272c36df5d9478c8735bada
PauloVToledo <paulovicentetoledosalgado@gmail.com>
2023-10-25 22:03:53 -0300

    Este es mi primer commit. Dejo creado el repositorio para no ocupar tiempo en la clase. Pdta: utilizo como base de modelo la tarea de intranet. Y si es necesario agregar clases, las agrego en el UML y luego las creo en el proyecto.

---
+++ README.md
@@ -0,0 +1,3 @@
+Desarrollo del taller 2 de progra.
+
+Basado en modelos UML vistos en clases y tareas.
---
+++ src/main/java/launcher/Launcher.java
@@ -0,0 +1,13 @@
+package launcher;
+/*   Primer commit solo clase principal.
+import views.paginaPrincipalView;
+
+public class Launcher {
+    public static void main(String[] args) {
+        inicializar();
+    }
+    public static void inicializar(){
+        new paginaPrincipalView();
+    }
+}
+*/
